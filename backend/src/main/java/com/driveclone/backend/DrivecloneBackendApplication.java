fd131f93fd0f8cf3886e8a1833f38c4f8ad015f1
MeerNadeemudeen-rec <meernadeemudeen.2024.aiml@rajalakshmi.edu.in>
2026-06-21 12:25:56 +0530

    Initial Spring Boot project setup

---
+++ backend/src/main/java/com/driveclone/backend/DrivecloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.driveclone.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivecloneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivecloneBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/driveclone/backend/DrivecloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driveclone.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivecloneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
